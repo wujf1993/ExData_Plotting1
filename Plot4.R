39b797ddd0b3e6620eaffd3f5befb56cd640c442
wujf1993 <pepper.chen@gmail.com>
2015-01-11 17:51:56 -0500

    Updated

--- Plot4.R
+++ Plot4.R
@@ -1,3 +1,4 @@
+
 library(data.table)
 house_energy = fread("C:\\Users\\Wu\\Downloads\\household_power_consumption.txt")
 house_cut <- house_energy[house_energy$Date == '1/2/2007' | house_energy$Date == '2/2/2007',]
